377e87ba61ed7a1710907901be865e2bca0a792f
ArtemErshov2004 <mr.artem.ershov.2004@mail.ru>
2024-02-29 14:21:29 +0300

    DG

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        public List<Hotel> CurrentHotel { get; set; }
+        public List<Holel> CurrentHotels {  get; set; }
 
         public MainWindow()
         {
