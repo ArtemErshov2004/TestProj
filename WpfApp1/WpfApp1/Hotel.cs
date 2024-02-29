5845f5fd4ea003878d91c28c0f7c0f74b08b6fe4
ArtemErshov2004 <mr.artem.ershov.2004@mail.ru>
2024-02-29 14:06:35 +0300

    DataGrid

--- WpfApp1/WpfApp1/Hotel.cs
+++ WpfApp1/WpfApp1/Hotel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WpfApp1
 {
-    public class Hotel
+    internal class Hotel
     {
         public int Id { get; set; }
         public string Name { get; set; }
